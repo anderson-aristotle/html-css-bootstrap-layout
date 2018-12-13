daabb219bc2645a756e79176d9071c5ddfad7433
Erica Salling <esalling23@gmail.com>
2018-12-13 15:55:28 -0500

    Run command line tasks
    Changed grey to $grey in index.scss
    Commented-out getFormFields declaration to appease grunt linter
    Added line about uncommenting getFormFields for dev instructions
    
    Refresh for 029

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,6 +1,7 @@
-const getFormFields = require('../../lib/get-form-fields')
+// const getFormFields = require('../../lib/get-form-fields')
 // Using your knowledge of jQuery write a function, onSubmitForm, that console
 // logs the input in the input field when "save changes" is clicked
+// you'll need to uncomment the first line to use getFormFields
 
 module.exports = {
 }
